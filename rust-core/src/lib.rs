bee8e602cf8a82e15859ea3bc2a2f0162a62961b
JdC <theodoreyong9@gmail.com>
2026-08-15 00:45:36 +0200

    Add modules module and update public exports

--- rust-core/src/lib.rs
+++ rust-core/src/lib.rs
@@ -4,6 +4,7 @@ mod dag;
 mod economics;
 mod event;
 mod identity;
+mod modules;
 
 pub use conservation::{identity_derivation, Claim, ClaimStatus, ConservationError, ConservationState, DerivationFn, Proof};
 pub use core::EventDagCore;
@@ -17,4 +18,11 @@ pub use event::Event;
 pub use identity::{
     verify_burn_proof, Commitment, IdentityCostState, NormalizedBurnTx, RegisterResult,
     RegisteredIdentity, VerifyResult, SOLANA_INCINERATOR_ADDRESS,
+};
+pub use modules::{
+    build_submission_event, compute_module_hash, record_nonce, select_identity_scheme,
+    submit_module, validate_economic_config, validate_submission, verify_module_integrity,
+    verify_submission_signature, AuditStatus, EconomicConfig, IdentityScheme, ModuleEntry,
+    ModuleRegistryState, NewModule, RegisterOutcome, SubmissionEvent, SubmissionState,
+    SubmitOutcome, ValidationOutcome, ValidationResult,
 };
