b20ff8a70be840050a56dedfdfcde5631b20105a
JdC <theodoreyong9@gmail.com>
2026-08-15 11:12:04 +0200

    Add module_rank to modules in mod.rs

--- rust-core/src/modules/mod.rs
+++ rust-core/src/modules/mod.rs
@@ -1,9 +1,11 @@
 pub mod module_hash;
+pub mod module_rank;
 pub mod module_registry;
 pub mod module_registry_reducer;
 pub mod module_submission;
 
 pub use module_hash::{compute_module_hash, verify_module_integrity};
+pub use module_rank::{check_submission_eligibility, compute_module_rank, EligibilityResult, LastSubmission};
 pub use module_registry::{
     select_identity_scheme, validate_economic_config, AuditStatus, EconomicConfig, IdentityScheme,
     ModuleEntry, ModuleRegistryState, NewModule, RegisterOutcome, ValidationResult,
