5daadcefd189061aae5813d1ec71ecb1f44925b0
JdC <theodoreyong9@gmail.com>
2026-08-15 22:24:23 +0200

    Delete scripts/check-id-parity.mjs

--- scripts/check-id-parity.mjs
+++
@@ -1,9 +0,0 @@
-pub mod cadence;
-pub mod g;
-pub mod reward;
-pub mod scarcity;
-
-pub use cadence::{CadenceState, DomainCadenceState};
-pub use g::{AccrualRejection, GState, Theta};
-pub use reward::{elapsed_epochs, reward, RewardParams};
-pub use scarcity::{simulate_hourly_issuance, DomainConfig, DomainScarcityState, ScarcityState, Snapshot};
