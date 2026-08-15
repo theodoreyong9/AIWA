e29bbf28268c09fc43a294685d3b19757bb168e7
JdC <theodoreyong9@gmail.com>
2026-08-15 10:45:12 +0200

    Add module_registry_reducer to mod.rs

--- rust-core/src/modules/mod.rs
+++ rust-core/src/modules/mod.rs
@@ -1,5 +1,6 @@
 pub mod module_hash;
 pub mod module_registry;
+pub mod module_registry_reducer;
 pub mod module_submission;
 
 pub use module_hash::{compute_module_hash, verify_module_integrity};
@@ -7,6 +8,7 @@ pub use module_registry::{
     select_identity_scheme, validate_economic_config, AuditStatus, EconomicConfig, IdentityScheme,
     ModuleEntry, ModuleRegistryState, NewModule, RegisterOutcome, ValidationResult,
 };
+pub use module_registry_reducer::{apply_module_event, materialize_module_registry};
 pub use module_submission::{
     build_submission_event, record_nonce, submit_module, validate_submission,
     verify_submission_signature, SubmissionEvent, SubmissionState, SubmitOutcome, ValidationOutcome,
