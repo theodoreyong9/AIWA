0acb3d7d7d3adf3d1285c86a2b903b11cbf705a8
JdC <theodoreyong9@gmail.com>
2026-08-16 15:55:32 +0200

    Update mod.rs

--- rust-core/src/economics/mod.rs
+++ rust-core/src/economics/mod.rs
@@ -1,9 +1,14 @@
 pub mod cadence;
+pub mod formula_registry_reducer;
 pub mod g;
 pub mod reward;
 pub mod scarcity;
 
 pub use cadence::{CadenceState, DomainCadenceState};
+pub use formula_registry_reducer::{
+    apply_formula_event, materialize_formulas, FormulaParams, FormulaRegistryState,
+    FormulaRejection, MintedFormula, GENESIS_FORMULA_ID, GENESIS_FORMULA_PARAMS,
+};
 pub use g::{AccrualRejection, GState, Theta};
 pub use reward::{elapsed_epochs, reward, RewardParams};
 pub use scarcity::{simulate_hourly_issuance, DomainConfig, DomainScarcityState, ScarcityState, Snapshot};
