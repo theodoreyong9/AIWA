16e97382a0cc4d3f1a192a189590f2cb3bae9520
JdC <theodoreyong9@gmail.com>
2026-08-18 12:23:04 +0200

    Add rank_from_identity_and_cadence to module_rank exports

--- rust-core/src/modules/mod.rs
+++ rust-core/src/modules/mod.rs
@@ -5,7 +5,7 @@ pub mod module_registry_reducer;
 pub mod module_submission;
 
 pub use module_hash::{compute_module_hash, verify_module_integrity};
-pub use module_rank::{check_submission_eligibility, compute_module_rank, EligibilityResult, LastSubmission};
+pub use module_rank::{check_submission_eligibility, compute_module_rank, rank_from_identity_and_cadence, EligibilityResult, LastSubmission};
 pub use module_registry::{
     select_identity_scheme, validate_economic_config, AuditStatus, EconomicConfig, IdentityScheme,
     ModuleEntry, ModuleRegistryState, NewModule, RegisterOutcome, ValidationResult,
